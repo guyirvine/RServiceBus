dadacb1137358af9c6f7e0a218f0b9861f79c1d3
Guy Irvine <guy@guyirvine.com>
2013-04-09 07:06:35 +1200

    Added transaction hooks to mysql drivers

--- lib/rservicebus/AppResource/FluidDbMysql2.rb
+++ lib/rservicebus/AppResource/FluidDbMysql2.rb
@@ -9,6 +9,21 @@ module RServiceBus
             return FluidDb::Mysql2.new( uri )
         end
 
+        # Transaction Semantics
+        def Begin
+            @connection.execute( "BEGIN", [] )
+        end
+        
+        # Transaction Semantics
+        def Commit
+            @connection.execute( "COMMIT", [] )
+        end
+        
+        # Transaction Semantics
+        def Rollback
+            @connection.execute( "ROLLBACK", [] )
+        end
+        
     end
     
 end
