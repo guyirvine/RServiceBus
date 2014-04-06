a58fee273265293a641bcf92ef5a03db9adc2b93
Ryan French <rfrench@lic.co.nz>
2014-04-07 10:55:41 +1200

    Added environment variable checking if nil and for true/false values

--- lib/rservicebus/Message.rb
+++ lib/rservicebus/Message.rb
@@ -1,36 +1,36 @@
 module RServiceBus
-    
+
     require "zlib"
     require "yaml"
     require "uuidtools"
 
     #This is the top level message that is passed around the bus
     class Message
-        
+
         attr_reader :returnAddress, :msgId, :remoteQueueName, :remoteHostName, :lastErrorSourceQueue, :lastErrorString, :correlationId, :sendAt
-        
+
         # Constructor
         #
         # @param [Object] msg The msg to be sent
         # @param [Object] returnAddress A queue to which the destination message handler can send replies
         def initialize( msg, returnAddress, correlationId=nil )
-            if ENV["RSBMSG_COMPRESS"].nil? then
+            if !RServiceBus.checkEnvironmentVariable(ENV['RSBMSG_COMPRESS']) then
                 @compressed = false
                 @_msg=YAML::dump(msg)
                 else
                 @compressed = true
                 @_msg=Zlib::Deflate.deflate(YAML::dump(msg))
             end
-            
+
             @correlationId = correlationId
             @returnAddress=returnAddress
-            
+
             @createdAt = DateTime.now
-            
+
             @msgId=UUIDTools::UUID.random_create
             @errorList = Array.new
         end
-        
+
         # If an error occurs while processing the message, this method allows details of the error to held
         # next to the msg.
         #
@@ -42,22 +42,22 @@ module RServiceBus
         def addErrorMsg( sourceQueue, errorString )
             @lastErrorSourceQueue = sourceQueue
             @lastErrorString = errorString
-            
+
             @errorList << RServiceBus::ErrorMessage.new( sourceQueue, errorString )
         end
-        
+
         def setRemoteHostName( hostName )
             @remoteHostName = hostName
         end
-        
+
         def setRemoteQueueName( queueName )
             @remoteQueueName = queueName
         end
-        
+
         def sendAt( timestamp )
             @sendAt = timestamp
         end
-        
+
         # @return [Object] The msg to be sent
         def msg
             if @compressed == true then
@@ -67,13 +67,13 @@ module RServiceBus
             end
             rescue ArgumentError => e
             raise e if e.message.index( "undefined class/module " ).nil?
-            
+
             puts e.message
             msg_name = e.message.sub( "undefined class/module ", "" )
-            
+
             raise ClassNotFoundForMsg.new( msg_name )
         end
-        
+
     end
-    
+
 end
--- lib/rservicebus/helper_functions.rb
+++ lib/rservicebus/helper_functions.rb
@@ -1,59 +1,59 @@
 module RServiceBus
-    
-    
+
+
 	def RServiceBus.convertDTOToHash( obj )
 		hash = {};
 		obj.instance_variables.each {|var| hash[var.to_s.delete("@")] = obj.instance_variable_get(var) }
-        
+
 		return hash
 	end
-    
+
 	def RServiceBus.convertDTOToJson( obj )
 		hash = RServiceBus.convertDTOToHash(obj)
-        
+
 		return hash.to_json
 	end
-    
+
 	def RServiceBus.log(string, ver=false)
-        return unless ENV['TESTING'].nil?
-        
+        return if RServiceBus.checkEnvironmentVariable(ENV["TESTING"])
+
 		type = ver ? "VERB" : "INFO"
-        if !ENV["VERBOSE"].nil? || !ver then
+        if RServiceBus.checkEnvironmentVariable(ENV["VERBOSE"]) || !ver then
             timestamp = Time.new.strftime( "%Y-%m-%d %H:%M:%S" )
             puts "[#{type}] #{timestamp} :: #{string}"
         end
 	end
-    
+
     def RServiceBus.rlog(string)
-        if !ENV["RSBVERBOSE"].nil? then
+        if RServiceBus.checkEnvironmentVariable(ENV["RSBVERBOSE"]) then
             timestamp = Time.new.strftime( "%Y-%m-%d %H:%M:%S" )
             puts "[RSB] #{timestamp} :: #{string}"
         end
     end
-    
+
     def RServiceBus.createAnonymousClass( name_for_class )
         newAnonymousClass = Class.new(Object)
         Object.const_set( name_for_class, newAnonymousClass )
         return Object.const_get( name_for_class ).new
     end
-    
+
     def RServiceBus.getValue( name, default=nil )
         value = ( ENV[name].nil?  || ENV[name] == ""  ) ? default : ENV[name];
         log "Env value: #{name}: #{value}"
         return value
     end
-    
+
     def RServiceBus.sendMsg( msg, responseQueue="agent" )
         require "rservicebus/EndpointMapping"
         endpointMapping = EndpointMapping.new
         endpointMapping.Configure
         queueName = endpointMapping.get( msg.class.name )
-        
+
         ENV["RSBMQ"] = "beanstalk://localhost" if ENV["RSBMQ"].nil?
         agent = RServiceBus::Agent.new
         Audit.new( agent ).audit( msg )
         agent.sendMsg(msg, queueName, responseQueue)
-        
+
         rescue QueueNotFoundForMsg=>e
 		msg = "\n"
 		msg = "#{msg}*** Queue not found for, #{e.message}\n"
@@ -61,18 +61,18 @@ module RServiceBus
 		msg = "#{msg}*** MESSAGE_ENDPOINT_MAPPINGS=#{e.message}:<QueueName>\n"
 		raise StandardError.new( msg )
     end
-    
+
     def RServiceBus.sendMsg( msg, responseQueue="agent" )
         require "rservicebus/EndpointMapping"
         endpointMapping = EndpointMapping.new
         endpointMapping.Configure
         queueName = endpointMapping.get( msg.class.name )
-        
+
         ENV["RSBMQ"] = "beanstalk://localhost" if ENV["RSBMQ"].nil?
         agent = RServiceBus::Agent.new
         Audit.new( agent ).auditOutgoing( msg )
         agent.sendMsg(msg, queueName, responseQueue)
-        
+
         rescue QueueNotFoundForMsg=>e
 		msg = "\n"
 		msg = "#{msg}*** Queue not found for, #{e.message}\n"
@@ -80,19 +80,24 @@ module RServiceBus
 		msg = "#{msg}*** MESSAGE_ENDPOINT_MAPPINGS=#{e.message}:<QueueName>\n"
 		raise StandardError.new( msg )
     end
-    
+
     def RServiceBus.checkForReply( queueName )
         ENV["RSBMQ"] = "beanstalk://localhost" if ENV["RSBMQ"].nil?
         agent = RServiceBus::Agent.new
         msg = agent.checkForReply( queueName )
         Audit.new( agent ).auditIncoming( msg )
-        
+
         return msg
     end
-    
+
     def RServiceBus.tick( string )
         puts "[TICK] #{Time.new.strftime( '%Y-%m-%d %H:%M:%S.%6N' )} :: #{caller[0]}. #{string}"
     end
-    
-    
+
+    def RServiceBus.checkEnvironmentVariable( string )
+        return false if ENV[string].nil?
+        return true if ENV[string] == true || ENV[string] =~ (/(true|t|yes|y|1)$/i)
+        return false if ENV[string] == false || ENV[string].blank? || ENV[string] =~ (/(false|f|no|n|0)$/i)
+        raise ArgumentError.new("invalid value for Environment Variable: \"#{string}\"")
+    end
 end
