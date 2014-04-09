2cd270f1628ce19a7392455b1741dd80d0c230b3
Ryan French <rfrench@lic.co.nz>
2014-04-10 10:42:26 +1200

    Changed SCP delete to use a regex for matching files rather than the glob style

--- lib/rservicebus/AppResource/ScpUpload.rb
+++ lib/rservicebus/AppResource/ScpUpload.rb
@@ -3,49 +3,49 @@ require 'net/sftp'
 
 module RServiceBus
 
-    class ScpUploadHelper
-        attr_reader :uri
+  class ScpUploadHelper
+    attr_reader :uri
 
-        def initialize( uri )
-            @uri = uri
-        end
-
-        def upload( source )
-            #opportunity for smarts here. Could tar zip if it was a directory of files
-
-#            Net::SCP.upload!(@uri.host, @uri.user, source, @uri.path, :recursive )
-		RServiceBus.log "Host: #{@uri.host}, User: #{@uri.user}, Source: #{source}, Destination: #{@uri.path}", true
-		Net::SSH.start( @uri.host, @uri.user ) do|ssh|
-			ssh.scp.upload!( source, @uri.path, :recursive => true )
-		end
-
-        end
-
-        def close
-        end
-
-        def delete( path, filepattern )
-            RServiceBus.log "Host: #{@uri.host}, User: #{@uri.user}, File Pattern: #{filepattern}, Destination: #{@uri.path}", true
-            Net::SSH.start( @uri.host, @uri.user ) do |ssh|
-                ssh.sftp.connect do |sftp|
-                    sftp.dir.glob(path, filepattern) {
-                        |file|
-                        sftp.remove("#{path}/#{file.name}")
-                    }
-                end
-            end
-        end
-
+    def initialize( uri )
+      @uri = uri
     end
 
-    class AppResource_ScpUpload<AppResource
+    def upload( source )
+      #opportunity for smarts here. Could tar zip if it was a directory of files
 
-        def connect(uri)
-            return ScpUploadHelper.new( uri )
-
-            return inputDir;
-        end
-
+      #Net::SCP.upload!(@uri.host, @uri.user, source, @uri.path, :recursive )
+      RServiceBus.log "Host: #{@uri.host}, User: #{@uri.user}, Source: #{source}, Destination: #{@uri.path}", true
+      Net::SSH.start( @uri.host, @uri.user ) do|ssh|
+        ssh.scp.upload!( source, @uri.path, :recursive => true )
+      end
     end
 
+    def close
+    end
+
+    def delete( path, filepattern )
+      RServiceBus.log "Host: #{@uri.host}, User: #{@uri.user}, File Pattern: #{filepattern}, Destination: #{@uri.path}", true
+      Net::SSH.start( @uri.host, @uri.user ) do |ssh|
+        ssh.sftp.connect do |sftp|
+          # sftp.dir.glob(path, filepattern) {
+          #   |file|
+          #   sftp.remove("#{path}/#{file.name}")
+          # }
+          sftp.dir.foreach(path) dp |entry|
+            puts entry.name
+          end
+        end
+      end
+    end
+  end
+
+  class AppResource_ScpUpload<AppResource
+
+    def connect(uri)
+      return ScpUploadHelper.new( uri )
+
+      return inputDir;
+    end
+
+  end
 end
